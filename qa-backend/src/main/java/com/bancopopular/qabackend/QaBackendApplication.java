f023e3de486b7f9c9d502c6faecb43c0b39a8a34
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-18 11:38:39 -0400

    initial setup

---
+++ qa-backend/src/main/java/com/bancopopular/qabackend/QaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bancopopular.qabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaBackendApplication.class, args);
+	}
+
+}
---
+++ qa-backend/src/test/java/com/bancopopular/qabackend/QaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancopopular.qabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
