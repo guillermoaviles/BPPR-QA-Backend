088b0d9e09fc25bb46402c0e9153ba3ca4717e77
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-18 16:22:55 -0400

    environment-through-username

--- qa-backend/src/main/java/com/bancopopular/qabackend/controller/impl/ProfileDataController.java
+++ qa-backend/src/main/java/com/bancopopular/qabackend/controller/impl/ProfileDataController.java
@@ -5,7 +5,10 @@ import com.bancopopular.qabackend.model.ProfileData;
 import com.bancopopular.qabackend.repository.ProfileDataRepository;
 import com.bancopopular.qabackend.service.impl.ProfileDataService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,9 +22,43 @@ public class ProfileDataController implements IProfileDataController {
     @Autowired
     ProfileDataService profileDataService;
 
-
+    // **************************************************** GET ***********************************************
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
     public List<ProfileData> getAllProfiles(){
         return profileDataRepository.findAll();
     }
+    @GetMapping("/searchByEnvironment/{environment}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProfileData> getProfilesByEnvironment(String environment) {
+        return profileDataRepository.findByEnvironment(environment);
+    }
+    @GetMapping("/searchByIntendedUse/{intendedUse}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProfileData> getProfilesByIntendedUse(String intendedUse) {
+        return profileDataRepository.findByIntendedUse(intendedUse);
+    }
+    @GetMapping("/searchByInUse/{inUse}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProfileData> getProfilesByInUse(boolean inUse) {
+        return profileDataRepository.findByInUse(inUse);
+    }
+    @GetMapping("/searchByProfileUserId/{profileUserId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProfileData getProfilesByProfileUserId(String profileUserId) {
+        return profileDataRepository.findByProfileUserId(profileUserId);
+    }
+    @GetMapping("/searchByUsername/{profileUserId}")
+    @ResponseStatus(HttpStatus.OK)
+    public ProfileData getProfilesByUsername(String username) {
+        return profileDataRepository.findByUsername(username);
+    }
 
+    // **************************************************  POST  ******************************************************
+
+    // **************************************************  PUT  *******************************************************
+
+    // *************************************************  PATCH  ******************************************************
+
+    //  ***********************************************  DELETE  ******************************************************
 }
--- qa-backend/src/main/java/com/bancopopular/qabackend/repository/ProfileDataRepository.java
+++ qa-backend/src/main/java/com/bancopopular/qabackend/repository/ProfileDataRepository.java
@@ -4,6 +4,13 @@ import com.bancopopular.qabackend.model.ProfileData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfileDataRepository extends JpaRepository<ProfileData, String> {
+    List<ProfileData> findByEnvironment(String environment);
+    List<ProfileData> findByIntendedUse(String intendedUse);
+    List<ProfileData> findByInUse(boolean inUse);
+    ProfileData findByProfileUserId(String profileUserId);
+    ProfileData findByUsername(String username);
 }
