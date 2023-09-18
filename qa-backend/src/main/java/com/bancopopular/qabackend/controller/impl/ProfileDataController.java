38cbb5122e18e8db953c5d2a300fdbb0726b1d2e
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-18 14:34:44 -0400

    branch out

--- qa-backend/src/main/java/com/bancopopular/qabackend/controller/impl/ProfileDataController.java
+++ qa-backend/src/main/java/com/bancopopular/qabackend/controller/impl/ProfileDataController.java
@@ -1,15 +1,27 @@
 package com.bancopopular.qabackend.controller.impl;
 
 import com.bancopopular.qabackend.controller.interfaces.IProfileDataController;
+import com.bancopopular.qabackend.model.ProfileData;
+import com.bancopopular.qabackend.repository.ProfileDataRepository;
 import com.bancopopular.qabackend.service.impl.ProfileDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/profiles")
 public class ProfileDataController implements IProfileDataController {
 
     @Autowired
+    ProfileDataRepository profileDataRepository;
+    @Autowired
     ProfileDataService profileDataService;
+
+
+    public List<ProfileData> getAllProfiles(){
+        return profileDataRepository.findAll();
+    }
+
 }
