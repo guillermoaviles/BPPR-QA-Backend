184657f5b3b9bb68f118b6c3537337cb760ee601
Guillermo Aviles <gjaviles7@gmail.com>
2023-09-27 12:38:28 -0400

    calls ready

--- qa-backend/src/main/java/com/bancopopular/qabackend/service/impl/ProfileDataService.java
+++ qa-backend/src/main/java/com/bancopopular/qabackend/service/impl/ProfileDataService.java
@@ -104,6 +104,7 @@ public class ProfileDataService implements IProfileDataService {
         if (profileDataOptional.isPresent()){
             ProfileData profileData = profileDataOptional.get();
             profileData.setInUse(profileDataDTO.isInUse());
+            profileData.setUser(profileDataDTO.getUser());
             profileDataRepository.save(profileData);
             logger.info("Successfully updated in use for profile with ID: {}", id);
         } else {
